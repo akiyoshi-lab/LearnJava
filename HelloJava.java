5eaf6aa3a3bdd07800a64ad760922f23fb047e67
AkihroTajiam <32767695+AkihiroTajima@users.noreply.github.com>
2019-04-16 17:33:35 +0900

    Create HelloJava.java

---
+++ HelloJava.java
@@ -0,0 +1,15 @@
+// This file is a "Hello, world!" in Java language by OpenJDK
+
+class HelloJava
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Wandbox!");
+    }
+}
+
+// OpenJDK reference:
+//   http://openjdk.java.net/
+
+// Java language references:
+//   http://docs.oracle.com/javase
